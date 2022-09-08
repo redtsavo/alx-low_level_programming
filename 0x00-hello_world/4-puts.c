18f9c97c6a9960815407491bfe8777f9a41b5c31
redtsavo <redtsavo@gmail.com>
2022-09-08 06:41:44 -0700

    C programming basics task 04

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingial puzzle");
+	return (0);
+}
