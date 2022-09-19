35a4b3ab3518f55ce8567897a47f4b91506407c1
redtsavo <redtsavo@gmail.com>
2022-09-19 06:31:09 -0700

    Nested loops and functions task 00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *  _isupper - checks for uppercase character
- *  @c: character to be checked
- *  Return: 1 if true, 0 if false
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
  */
 
 int _isupper(int c)
