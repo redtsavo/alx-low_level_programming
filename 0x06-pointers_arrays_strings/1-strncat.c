fbe567fbd3390f12967d79352b0f931d625ea91c
redtsavo <redtsavo@gmail.com>
2022-09-22 08:35:03 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  *_strncat - concatenate two strings but add inputted number of bytes
  *@dest: string to be appended upon
  *@src: string to be completed at the end of dest
- *@n:integer parameter to compare index to
+ *@n: integer parameter to compare index to
  *Return: returns new concatenated string
  */
 
